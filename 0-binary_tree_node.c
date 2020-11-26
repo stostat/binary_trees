72613b38925a5cd9a549fe11011b976e6a86f659
stostat <santmendieta@icloud.com>
2020-11-26 17:27:00 -0500

    first node, added function for node inserttion

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,24 @@
+#include "binary_trees.h"
+
+
+/**
+ * binary_tree_node - creates a new node from root given address
+ *
+ * @parent: Pointer to the node
+ *
+ * @value: int to insert  into the node
+ *
+ * Return: Pointer ro node
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *temp = malloc(sizeof(binary_tree_t));
+
+	parent = temp;
+	parent->n = value;
+	parent->left = NULL;
+	parent->right = NULL;
+
+	return (parent);
+
+}
